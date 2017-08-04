f0b1810aa960995ae0835df325b77c805ffe52a7
mohaabbas <mohaalazzawy@gmail.com>
2017-08-04 23:18:44 +0200

    upload the project

---
+++ app/src/main/java/com/example/moha/cardrac/Card.java
@@ -0,0 +1,20 @@
+package com.example.moha.cardrac;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Moha on 2017-02-05.
+ */
+
+public class Card {
+
+    int value, image;
+
+    public Card(int value, int image) {
+
+        this.value = value;
+        this.image = image;
+    }
+
+}
